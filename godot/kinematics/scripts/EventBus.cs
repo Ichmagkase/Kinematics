a0a5965e29a566f4f262ef2bb3171f831470a999
Dhruv Patel <169485603+dhruvp987@users.noreply.github.com>
2026-03-27 19:25:45 -0400

    fix: remove namespace from autoloaded EventBus

--- godot/kinematics/scripts/EventBus.cs
+++ godot/kinematics/scripts/EventBus.cs
@@ -1,26 +1,23 @@
 using Godot;
 using System;
 
-namespace Game.Events
+public partial class EventBus : Node
 {
-	public partial class EventBus : Node
+	[Signal]
+	public delegate void PlayerOneActionEventHandler(string action);
+
+	[Signal]
+	public delegate void PlayerTwoActionEventHandler(string action);
+
+	public static EventBus Instance { get; private set; }
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
 	{
-		[Signal]
-		public delegate void PlayerOneActionEventHandler(string action);
+	}
 
-		[Signal]
-		public delegate void PlayerTwoActionEventHandler(string action);
-
-		public static EventBus Instance { get; private set; }
-
-		// Called when the node enters the scene tree for the first time.
-		public override void _Ready()
-		{
-		}
-
-		// Called every frame. 'delta' is the elapsed time since the previous frame.
-		public override void _Process(double delta)
-		{
-		}
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
 	}
 }
