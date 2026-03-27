221606470a249fd02d06279a889c237922ab8036
Dhruv Patel <169485603+dhruvp987@users.noreply.github.com>
2026-03-27 19:19:43 -0400

    task: create ProcessCreator that creates bridge process

---
+++ godot/kinematics/scripts/ProcessCreator.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class ProcessCreator : Node
+{
+	private const string KINECT_BRIDGE_FILENAME = "KinectVisionLayer.exe";
+
+	private string CreatePathInExecDir(string filename)
+	{
+		return OS.GetExecutablePath().GetBaseDir() + "/" + filename;
+	}
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// Only automatically create bridge process if running exported executable.
+		if (!OS.HasFeature("editor"))
+		{
+			string path = CreatePathInExecDir(KINECT_BRIDGE_FILENAME);
+			int pid = OS.CreateProcess(path, []);
+			if (pid == -1)
+			{
+				GD.PrintErr($"Could not create {KINECT_BRIDGE_FILENAME} process");
+			}
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
