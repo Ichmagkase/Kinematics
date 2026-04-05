da04f071b4c8f2f3c155f3e085e88ce3ba9917ba
Dhruv Patel <169485603+dhruvp987@users.noreply.github.com>
2026-04-05 01:41:15 -0400

    task: create UDP socket client for testing game UDP server

---
+++ testudpclient/player_action_client.py
@@ -0,0 +1,17 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 4242
+
+print(f"Host: {HOST}, Port: {PORT}")
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    while True:
+        playerId = input("Enter a player ID: ")
+        action = input("Enter an action: ")
+
+        message = (playerId + "|" + action).encode()
+
+        s.sendto(message, (HOST, PORT))
+
+        print(f"Sent message: {message}")
