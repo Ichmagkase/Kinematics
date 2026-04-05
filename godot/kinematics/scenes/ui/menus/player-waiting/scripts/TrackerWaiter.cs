cb46bda74b802eee93108390ca454b00eeab3900
Dhruv Patel <169485603+dhruvp987@users.noreply.github.com>
2026-04-05 00:31:24 -0400

    fix: make tracker waiter go to ready waiter when skipped

--- godot/kinematics/scenes/ui/menus/player-waiting/scripts/TrackerWaiter.cs
+++ godot/kinematics/scenes/ui/menus/player-waiting/scripts/TrackerWaiter.cs
@@ -15,6 +15,11 @@ public partial class TrackerWaiter : Node
 		_trackerIsReady = true;
 	}
 
+	public void _OnSkipButtonPressed()
+	{
+		EmitSignal(SignalName.TrackerReady);
+	}
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
