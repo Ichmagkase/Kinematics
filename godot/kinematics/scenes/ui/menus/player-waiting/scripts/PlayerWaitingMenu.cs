677c0bd8cbb6fd8f8fd61a744886f6e806cdc9a0
Dhruv Patel <169485603+dhruvp987@users.noreply.github.com>
2026-04-04 23:43:25 -0400

    task: add transition to countdown in top level menu

--- godot/kinematics/scenes/ui/menus/player-waiting/scripts/PlayerWaitingMenu.cs
+++ godot/kinematics/scenes/ui/menus/player-waiting/scripts/PlayerWaitingMenu.cs
@@ -6,8 +6,15 @@ public partial class PlayerWaitingMenu : Node
 	[Export]
 	public PackedScene WaitingPlayerReadyMenu;
 
+	[Export]
+	public PackedScene CountdownMenu;
+
+	[Export]
+	public PackedScene GameScene;
+
 	private TrackerWaiter _trackerWaiter;
 	private PlayerReadyWaiter _playerReadyWaiter;
+	private Countdown _countdown;
 
 	private void TrackerReadyHandler()
 	{
@@ -23,6 +30,14 @@ public partial class PlayerWaitingMenu : Node
 		ActivateCountdownScene();
 	}
 
+	private void CountdownFinishedHandler()
+	{
+		_countdown.CountdownFinished -= CountdownFinishedHandler;
+		_countdown.QueueFree();
+		// Start game
+		GetTree().ChangeSceneToPacked(GameScene);
+	}
+
 	private void ActivateWaitingPlayerReadyScene()
 	{
 		_playerReadyWaiter = WaitingPlayerReadyMenu.Instantiate<PlayerReadyWaiter>();
@@ -32,7 +47,9 @@ public partial class PlayerWaitingMenu : Node
 
 	private void ActivateCountdownScene()
 	{
-		// Create countdown scene
+		_countdown = CountdownMenu.Instantiate<Countdown>();
+		AddChild(_countdown);
+		_countdown.CountdownFinished += CountdownFinishedHandler;
 	}
 
 	// Called when the node enters the scene tree for the first time.
